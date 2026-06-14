cf3b166ccdbe893dcdd33eb7d0c8bd8d0ca71d32
David O Connor <davidstephen10@outlook.com>
2026-06-14 21:16:32 +0100

    Add login functionality and session creation

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask,render_template,redirect,url_for
-from forms import RegistrationForm
+from flask import Flask,render_template,redirect,url_for,session,g,request
+from forms import RegistrationForm,LoginForm
 from database import get_db,close_db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_session import Session
+from functools import wraps
 
 app = Flask(__name__)
+app.teardown_appcontext(close_db)
+app.config["SECRET_KEY"] = "my_secret_key"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def home():
@@ -14,8 +21,8 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = user.form.data
-        password = password.form.data
+        user = form.user.data
+        password = form.password.data
         db = get_db()
         conflict = db.execute("""SELECT * FROM users WHERE username = ?""",(user,)).fetchone()
 
@@ -26,3 +33,26 @@ def register():
             db.commit()
             return redirect(url_for("login"))
     return render_template("register.html",form=form)
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = form.user.data
+        password = form.password.data
+        db = get_db()
+        matching_user = db.execute("""SELECT * FROM users WHERE username = ?""",(user,)).fetchone()
+        if matching_user is None:
+            form.user.errors.append("Unknown user id")
+        elif not check_password_hash(matching_user["password"],password):
+            form.password.errors.append("Incorrect password")
+        else:
+            session.clear()
+            session["user"] = user
+            next_page = request.args.get("next")
+            if not next_page:
+                next_page = url_for("home")
+            return redirect(next_page)
+    return render_template("login.html",form=form)
+        
+    
--- forms.py
+++ forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField,StringField
+from wtforms import SubmitField,StringField,PasswordField
 from wtforms.validators import InputRequired,EqualTo
 
 class RegistrationForm(FlaskForm):
     user = StringField("Username:",validators=[InputRequired()])
-    password = StringField("Password:",validators=[InputRequired()])
-    password2 = StringField("Confirm password:",validators=[InputRequired(),EqualTo()])
+    password = PasswordField("Password:",validators=[InputRequired()])
+    password2 = PasswordField("Confirm password:",validators=[InputRequired(),EqualTo("password")])
     submit= SubmitField("Submit")
+
+class LoginForm(FlaskForm):
+    user = StringField("Username:",validators=[InputRequired()])
+    password = PasswordField("Password:",validators=[InputRequired()])
+    submit= SubmitField("Submit")
+    
